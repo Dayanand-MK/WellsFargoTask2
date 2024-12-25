ca1a758911138ca6d5dabc46a4bd5526c6e2627a
Dayanand-MK <dayanandmanikandan@yahoo.com>
2024-12-25 15:36:13 +0530

    Client.java

---
+++ Client.java
@@ -0,0 +1,32 @@
+package com.example.project.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    private String name;
+    private String email;
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private FinancialAdvisor financialAdvisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    public Client() {}
+
+    public Client(String name, String email, String phone, FinancialAdvisor financialAdvisor) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.financialAdvisor = financialAdvisor;
+    }
+
+    // Getters and Setters
+}
